9d75d8092a1f977298d68d6a66ae92e78ce39c73
jitaporn <66560781+jitaporn@users.noreply.github.com>
2026-07-02 23:02:09 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jos-pos-v1.7.1';
-const ASSETS=['./','./index.html','./style.css','./product.css','./app.js','./product.js','./features.js','./finance.js','./stock-engine.js','./stock-receive.js','./lot-engine.js','./receive-v2.js','./manifest.webmanifest'];
+const CACHE='jos-pos-v1.8.0';
+const ASSETS=['./','./index.html','./style.css','./product.css','./app.js','./product.js','./features.js','./finance.js','./stock-engine.js','./stock-receive.js','./lot-engine.js','./receive-v2.js','./ui-dialog.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(a=>Promise.all(a.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(x=>{let y=x.clone();caches.open(CACHE).then(c=>c.put('./index.html',y));return x}).catch(()=>caches.match('./index.html')));return}e.respondWith(fetch(e.request).then(x=>{let y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match(e.request))) });
