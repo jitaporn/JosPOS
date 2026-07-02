76782f8a8f64c747ea9b7395f8091897742aef2c
jitaporn <66560781+jitaporn@users.noreply.github.com>
2026-07-02 21:05:08 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jos-pos-v1.4.0';
+const CACHE='jos-pos-v1.4.1';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./product.css','./app.js','./product.js','./features.js','./finance.js','./stock-engine.js','./manifest.webmanifest']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
